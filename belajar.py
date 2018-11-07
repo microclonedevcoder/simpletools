6ec8f9d885c39501886160916afee076c6f8375d
MicroClone <43690001+MicroClone@users.noreply.github.com>
2018-11-07 22:58:35 +0700

    Add files via upload

---
+++ belajar.py
@@ -0,0 +1,63 @@
+import os
+import time
+
+red = '\033[31;1m'
+green = '\033[32;1m'
+yellow = '\033[33;1m'
+blue = '\033[34;1m'
+purple = '\033[35;1m'
+cyan = '\033[36;1m'
+white = '\033[37;1m'
+time.sleep(1)
+os.system('clear')
+
+print(white),(" author  : MicroClone")
+time.sleep(0.25)
+print(white),(" program : python")
+time.sleep(0.25)
+print(white),(" support : Learn Python group")
+time.sleep(0.25)
+print(white),(" contact : cybermicro7@gmail.com")
+time.sleep(0.25)
+print(white),(" thanks to : IndoSec")
+time.sleep(0.5)
+
+print("\n")
+
+print(blue),("____________________________________")
+time.sleep(0.1)
+print(white),("[ 1. ] all member group learn python")
+time.sleep(0.1)
+print(green),("____________________________________")
+time.sleep(0.1)
+print(white),("[ 0. ] exit")
+time.sleep(0.1)
+print""
+micro = input("\033[37;1m root@MicroClone~$ \033[37;1m")
+
+if micro == 1:
+	os.system('clear')
+	os.system("figlet Member Group |lolcat")
+	time.sleep(1.5)
+	os.system('clear')
+	os.system("figlet Learn Python |lolcat")
+	time.sleep(1.5)
+	os.system('clear')
+	os.system("echo -DarkOct02 -Karjok -MicroClone -TUAN B4DUT -SIT_GM |lolcat")
+	print""
+	os.system("echo -Mr_Tr3v!0n -Kingsman! -.//D357R0Y++ -Dfv47 -InYourHeart |lolcat")
+	print""
+	os.system("echo -InYourHeart -Kucing -L0N91NU5 -Droid -Mr.Pr0gr4mmer |lolcat")
+	time.sleep(3.5)
+	os.system('python2 belajar.py')
+
+if micro == 0:
+	os.system('clear')
+	os.system("echo good luck |lolcat")
+	time.sleep(2)
+	exit()
+
+else:
+	print(red),("wrong input")
+	time.sleep(0.5)
+	os.system('python2 belajar.py')
